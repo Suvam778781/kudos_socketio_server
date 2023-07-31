ca5ef4191ca46a52efb0363d4f1d4bd7cc0091d2
suvampanda <suvam.panda@devriser.com>
2023-07-31 20:10:23 +0530

    remove whole database and make socketio realtime apps

---
+++ utils/getCurrentDateTime.js
@@ -0,0 +1,10 @@
+function getCurrentDateTime() {
+    const currentDate = new Date();
+    const options = { weekday: 'long', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true };
+    const formattedDate = currentDate.toLocaleDateString('en-US', options);
+  
+    return formattedDate;
+  }
+  
+
+  module.exports={getCurrentDateTime}
