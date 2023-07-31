e2932255846aa29e2aa76bab4e6700c4e4d33956
suvampanda <suvam.panda@devriser.com>
2023-08-01 01:21:55 +0530

    added timezoned

--- utils/getCurrentDateTime.js
+++ utils/getCurrentDateTime.js
@@ -1,22 +1,29 @@
+const moment = require("moment-timezone");
+
 function getCurrentDateTime() {
-    const months = [
-      'January', 'February', 'March', 'April', 'May', 'June',
-      'July', 'August', 'September', 'October', 'November', 'December'
-    ];
-  
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  
-    const now = new Date();
-    const month = months[now.getMonth()];
-    const dayOfWeek = days[now.getDay()];
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-  
-    const amOrPm = hour >= 12 ? 'PM' : 'AM';
-    const formattedHour = hour % 12 || 12;
-  
-    const result = `${month} ${now.getFullYear()} ${dayOfWeek} at ${formattedHour}:${minute.toString().padStart(2, '0')} ${amOrPm}`;
-  
-    return result;
-  }
-  module.exports={getCurrentDateTime}
+  const timezone = "Asia/Kolkata"; // Set Kolkata as the default timezone
+
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
+  ];
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+  const now = moment().tz(timezone);
+  const month = months[now.month()];
+  const dayOfWeek = days[now.day()];
+  const hour = now.hour();
+  const minute = now.minute();
+
+  const amOrPm = hour >= 12 ? 'PM' : 'AM';
+  const formattedHour = hour % 12 || 12;
+
+  const result = `${month} ${now.year()} ${dayOfWeek} at ${formattedHour}:${minute.toString().padStart(2, '0')} ${amOrPm}`;
+
+  return result;
+}
+
+// Example usage:
+
+module.exports={getCurrentDateTime}
